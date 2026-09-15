4d420847b9edf51afdadbdbab6d660fcf9a0e76e
Davi Torres <davi.souza@facto.org.br>
2026-09-15 16:19:51 -0300

    atualizando arquitetura

--- src/browser.py
+++ src/browser.py
@@ -3,6 +3,25 @@
 A implementação concreta dependerá da biblioteca de automação escolhida
 (Selenium, Playwright ou equivalente).
 """
+def realizar_login():
+    #login aqui
+
+def buscar_contratos():
+    #funcao de selecionar tipo de contrato
+    #funcao de inserir nome do projeto
+    #funcao de limpeza de data
+    #funcao de buscar
+    #contrato = {href,projeto,tipo,situacao}
+
+    return lista_contratos
+
+def processar_projetos():
+    # pra cada pagina que existir
+    # verifica situacao do projeto
+    # abre o projeto
+    # clique em arquivos
+    # procure anexos e baixe todos os anexos
+    # coloque todos os anexos dentro da pasta do projeto/contrato
 
 
 def aguardar_loader_desaparecer(page, timeout=30):
